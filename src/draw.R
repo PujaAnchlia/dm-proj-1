144bb310bc73169348e7c634a49e123f96dcafc5
Dainis Boumber <dboumber@uh.edu>
2014-09-13 08:12:12 -0500

    Reorganized project

---
+++ src/draw.R
@@ -0,0 +1,11 @@
+draw_ <- function() {
+  banknoteData <- read.csv("data/data_banknote_authentication.txt",header=F)
+  class_ <-banknoteData$V5
+  print(class_)
+  temp_data <- banknoteData[c(-5)]
+  f_class <- factor(class_)
+  print(f_class)
+  banknoteData["V5"] <- f_class
+  #print(banknoteData)
+  plot(banknoteData$V1, banknoteData$V2,col=c("red","green")[banknoteData$V5])
+}
