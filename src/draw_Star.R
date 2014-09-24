f6d64c88bf54352dad17f9070e46dc53343a11a7
Dainis Boumber <dboumber@uh.edu>
2014-09-24 17:00:18 -0500

    fixed the star plots.

--- src/draw_Star.R
+++ src/draw_Star.R
@@ -18,18 +18,30 @@ draw_Star <- function(bnd_frame,label)
   
   counter <- 1
   output  <- matrix(nrow=20, ncol=4)
-  
+  labels <- vector()
+  locations <- matrix(nrow=20, ncol=2)
+  x = -20
+  y = 0
+  for (i in 1:5) {
+    for (j in 1:4) {
+      locations[counter, 1] <- x
+      locations[counter, 2] <- y
+      x = x + 10
+      counter = counter + 1
+    }
+    x = x - 40
+    y = y + 10
+  }
+  counter <- 1
   for (i in 1:20) {
-
       output[counter, 1] <- bnd_z_class[i,1]
       output[counter, 2] <- bnd_z_class[i,2]
       output[counter, 3] <- bnd_z_class[i,3]
       output[counter, 4] <- bnd_z_class[i,4]
-      
+      labels[i] <- counter
       counter = counter + 1
-
   }
-  
-  stars(output,main=paste("Star Plot - Class", label))
+  print(output)
+  stars(output,main=paste("Star Plot - Class", label), scale=FALSE, locations=locations, labels=labels)
   
 }
