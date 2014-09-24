9d802c306f1e76346bf8d92558dc99dabbe7ddf5
Dainis Boumber <dboumber@uh.edu>
2014-09-24 18:25:35 -0500

    draw tee

--- src/tree.R
+++ src/tree.R
@@ -1,6 +1,7 @@
 tree <- function(bnd_frame)
 {
   library(rpart)
+  library(rpart.plot)
   # Normalize the bank note dataset, excluding the class attribute
   sbnd <- data.frame(Var1=bnd_frame[,1],Skew1=bnd_frame[,2],
                      Kur1=bnd_frame[,3],Ent1=bnd_frame[,4])
@@ -12,9 +13,9 @@ tree <- function(bnd_frame)
   
   # Fit a tree model, to predict class attribute using the other 4 attributes
   # as the dependent variables.
-  lm_model <- rpart(norm_bnd_final$Class2 ~ norm_bnd_final$Var2 + norm_bnd_final$Skew2 + 
-                   norm_bnd_final$Kur2 + norm_bnd_final$Ent2)
+  model <- rpart(norm_bnd_final$Class2 ~ norm_bnd_final$Var2 + norm_bnd_final$Skew2 + 
+                   norm_bnd_final$Kur2 + norm_bnd_final$Ent2, method="class",
+                   control=rpart.control(cp=0.03, maxdepth=4))
   
-  print(summary(lm_model))
-  
+  prp(model)
 }
