a9aa71977f690d5bdf294e7b78e3ead3667c6c3d
Dainis Boumber <dboumber@uh.edu>
2014-09-24 18:40:53 -0500

    variable names changed

--- src/tree.R
+++ src/tree.R
@@ -8,13 +8,13 @@ tree <- function(bnd_frame)
   normal_bnd <- scale(sbnd)
   
   # Add back the class attribute
-  norm_bnd_final <- data.frame(Var2=normal_bnd[,1],Skew2=normal_bnd[,2],
-                               Kur2=normal_bnd[,3],Ent2=normal_bnd[,4],Class2=bnd_frame[,5])
-  
+  norm_bnd_final <- data.frame(NormalVariance=normal_bnd[,1],NormalSkewness=normal_bnd[,2],
+                               NormalKurtosis=normal_bnd[,3],NormalEntropy=normal_bnd[,4],Class=bnd_frame[,5])
+  attach(norm_bnd_final)
+  on.exit(detach(norm_bnd_final),add = TRUE)
   # Fit a tree model, to predict class attribute using the other 4 attributes
   # as the dependent variables.
-  model <- rpart(norm_bnd_final$Class2 ~ norm_bnd_final$Var2 + norm_bnd_final$Skew2 + 
-                   norm_bnd_final$Kur2 + norm_bnd_final$Ent2, method="class",
+  model <- rpart(Class ~ NormalVariance + NormalSkewness + NormalKurtosis + NormalEntropy,method="class",
                    control=rpart.control(cp=0.03, maxdepth=4))
   
   prp(model)
